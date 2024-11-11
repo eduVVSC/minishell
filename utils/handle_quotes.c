9e38ae3a52ad8b981b998cd2edc089157af47d73
Diogo Filipe Ferreira E Silva <dioferre@c2r6s2.42porto.com>
2024-11-11 13:55:07 +0000

    fixed handle_quotes

--- utils/handle_quotes.c
+++ utils/handle_quotes.c
@@ -6,9 +6,11 @@
 /*   By: dioferre <dioferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 09:04:53 by dioferre          #+#    #+#             */
-/*   Updated: 2024/11/11 09:14:19 by dioferre         ###   ########.fr       */
+/*   Updated: 2024/11/11 13:53:40 by dioferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 /* Looks for the next quote type in the string,
 returns an index to that quote if found, -1 if none was found,
@@ -16,16 +18,16 @@ Flag: 0 if single quote; 1 if double quote. */
 int	handle_quotes(const char *str, int i, short flag)
 {
 	char	quote_type;
-	int		close_quote;
 
-	close_quote = 0;
+	i++;
 	if (flag == 0)
 		quote_type = '\'';
 	else
 		quote_type = '\"';
-	while (str[i + close_quote] != quote_type || str[i + close_quote] == '\0')
-		close_quote++;
-	if (str[i + close_quote] != quote_type)
+	printf("teste\n");
+	while (str[i] != quote_type && str[i] != '\0')
+		i++;
+	if (str[i] != quote_type)
 		return (-1);
-	return (close_quote);
+	return (i);
 }
