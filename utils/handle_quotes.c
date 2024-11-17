5dc65b9f0c86154bc33f0ec49f24d3e48729af98
Diogo Filipe Ferreira E Silva <dioferre@c2r4s4.42porto.com>
2024-11-17 18:25:19 +0000

    added wordcount to utils, need to make another util file later

--- utils/handle_quotes.c
+++ utils/handle_quotes.c
@@ -6,7 +6,7 @@
 /*   By: dioferre <dioferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 09:04:53 by dioferre          #+#    #+#             */
-/*   Updated: 2024/11/15 17:34:01 by dioferre         ###   ########.fr       */
+/*   Updated: 2024/11/17 15:26:54 by dioferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,24 @@ int	handle_quotes(const char *str, int i, char quote_type)
 	if (str[i] != quote_type)
 		return (-1);
 	return (i);
+}
+
+/* Counts words in a string based on a given separator
+returns word nr or 0 if str is empty. */
+int	word_count(char *str, char separator)
+{
+	int	i;
+	int	words;
+
+	i = 0;
+	words = 1;
+	if (!str || str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == separator)
+			words++;
+		i++;
+	}
+	return (words);
 }
