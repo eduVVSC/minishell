0d7bb6472019bc8b2c4a3f72ade7b6eaf41ff522
Diogo Filipe Ferreira E Silva <dioferre@c2r4s4.42porto.com>
2024-11-29 15:04:30 +0000

    updated minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dioferre <dioferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:12:10 by dioferre          #+#    #+#             */
-/*   Updated: 2024/11/25 17:00:44 by dioferre         ###   ########.fr       */
+/*   Updated: 2024/11/29 14:50:52 by dioferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 
 # define true 1
 # define false 0
+# define variable 1
+# define  value 0
 
 # include <stdio.h>
+#include <dirent.h>
 # include <signal.h>
+#include <sys/types.h>
 # include <readline/history.h>
 # include <readline/readline.h>
-# include "libft.h"
+# include "../libft/libft.h"
 
 typedef struct s_env
 {
@@ -30,6 +34,18 @@ typedef struct s_env
 	char	*path;
 }				t_env;
 
+typedef struct s_cmds
+{
+	char			**cmd;
+	struct s_cmds	*next;
+}				t_cmds;
+
+typedef struct s_tree_node
+{
+	void				*content;
+	struct s_tree_node	*left;
+	struct s_tree_node	*right;
+}					t_tree_node;
 
 typedef struct s_tokens
 {
@@ -52,7 +68,18 @@ void	print_tokens(t_tokens *tokens);
 void	giving_value_to_str(char *str, char *cmd, int strlen, int index_start);
 int		handle_quotes(const char *str, int i, char quote_type);
 int		word_count(char *str);
+char	**build_cmd_array(t_tokens *tokens);
+t_cmds	*build_cmd_list(char **cmds);
+void	print_cmds(char **cmds);
+void	cmd(char **command);
 
+
+// =============built-ins=============== //
+
+void	do_cd(char **command);
+void	do_pwd(char **command);
+void	do_ls(char **command);
+void	do_echo(char **command);
 
 // =============handlers=============== //
 void	cntrl_C();
@@ -63,6 +90,11 @@ void	signalHandler(int signum, siginfo_t *info, void *context);
 
 char	*fill_str(char *str, int target_word, char separator);
 
+// ============Expension============== //
 
+int		check_expand(char *str);
+char	*handle_expension(char *str);
+char	*var_name(char *str, int start);
+char	*re_wrinting_string(char *str, char *expand_value, int where_to_change, int var_len);
 
 #endif
