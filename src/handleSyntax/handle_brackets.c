63d559f5c65a0c222bf0c65efc20b9d72e56cd7f
Diogo Filipe Ferreira E Silva <dioferre@c2r4s4.42porto.com>
2024-12-02 17:21:53 +0000

    added check_open_quotes function

--- src/handleSyntax/handle_brackets.c
+++ src/handleSyntax/handle_brackets.c
@@ -6,7 +6,7 @@
 /*   By: dioferre <dioferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 12:37:40 by dioferre          #+#    #+#             */
-/*   Updated: 2024/11/19 12:37:41 by dioferre         ###   ########.fr       */
+/*   Updated: 2024/12/02 15:56:16 by dioferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,28 @@ int	check_open_brackets(const char *str)
 				j++;
 			if (str[i + j] == '\0')
 				return (true);
+		}
+		i++;
+	}
+	return (false);
+}
+
+/* Checks for open brackets in the string
+returns 1 if there are, 0 if not. */
+int	check_open_quotes(const char *str)
+{
+	// Might be just one pointer dont know yet lol.
+	char	quote_type;
+	int		i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+		{
+			if (handle_quotes(str, i, str[i]) == -1)
+				return (true);
+			i = handle_quotes(str, i, str[i]);
 		}
 		i++;
 	}
